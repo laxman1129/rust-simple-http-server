26270021110d89fab230e4252fb74a7f40430b6e
laxman1129 <laxman1129@gmail.com>
2025-08-18 21:32:35 +0400

    handler impl

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,10 +1,56 @@
-use crate::http::{Request, Response, StatusCode};
 use super::server::Handler;
+use crate::http::{Method, Request, Response, StatusCode};
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::OK,Some("<h1>Test Impl</h1>".to_string()))
+        // Response::new(StatusCode::OK,Some("<h1>Test Impl</h1>".to_string()))
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(
+                    StatusCode::OK,
+                    Some("<h1>Welcome to the Home Page</h1>".to_string()),
+                ),
+                "/about" => Response::new(StatusCode::OK, Some("<h1>About Us</h1>".to_string())),
+                "/contact" => {
+                    Response::new(StatusCode::OK, Some("<h1>Contact Us</h1>".to_string()))
+                }
+                _ => Response::new(
+                    StatusCode::NotFound,
+                    Some("<h1>Page Not Found</h1>".to_string()),
+                ),
+            },
+            Method::POST => match request.path() {
+                "/submit" => {
+                    Response::new(StatusCode::OK, Some("<h1>Form Submitted</h1>".to_string()))
+                }
+                _ => Response::new(
+                    StatusCode::NotFound,
+                    Some("<h1>Page Not Found</h1>".to_string()),
+                ),
+            },
+            Method::PUT => match request.path() {
+                "/update" => Response::new(
+                    StatusCode::OK,
+                    Some("<h1>Update Successful</h1>".to_string()),
+                ),
+                _ => Response::new(
+                    StatusCode::NotFound,
+                    Some("<h1>Page Not Found</h1>".to_string()),
+                ),
+            },
+            Method::DELETE => match request.path() {
+                "/delete" => Response::new(
+                    StatusCode::OK,
+                    Some("<h1>Delete Successful</h1>".to_string()),
+                ),
+                _ => Response::new(
+                    StatusCode::NotFound,
+                    Some("<h1>Page Not Found</h1>".to_string()),
+                ),
+            },
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
